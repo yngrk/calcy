77afe72e11b8c09a8bd823304ab9f303ab549c86
Young Rok Kim <young@Air-von-Young.fritz.box>
2022-06-23 19:44:00 +0200

    implements number button functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// define Elements
+const numBtn = document.querySelectorAll('.num');
+const lcdTop = document.querySelector('.lcd-history');
+const lcdBot = document.querySelector('.lcd-current');
+
 
 // OPERATIONS
 function add(a, b) {
@@ -39,3 +44,15 @@ function operate(operator, a, b) {
             return 'Err: unkown error'
     }
 }
+
+function printToScreen(str) {
+    lcdBot.textContent += `${str}`;
+}
+
+// add functionality to buttons 1-9:
+// populate display when buttons pressed
+// store the number in a variable when number ends
+// (e.g. when operation button pressed)
+numBtn.forEach(btn => btn.addEventListener('click', function() {
+    printToScreen(btn.textContent);
+}));
